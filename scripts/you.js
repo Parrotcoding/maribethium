86114934590df0d1ca18c04318633893c0c6c5d9
Parrotcoding <128106128+Parrotcoding@users.noreply.github.com>
2026-01-20 21:53:19 -0500

    Update you.js

--- scripts/you.js
+++ scripts/you.js
@@ -1,14 +1,11 @@
-container.addEventListener('click', async () => {
-	await proCreate(6);
-	window.onbeforeunload = () => "Are you annoying?";
-});
-
 window.onload = playBall;
 window.oncontextmenu = () => false;
-window.onkeydown = async () => {
+
+window.onkeydown = async (event) => {
 	if (['Control', 'Alt', 'Delete', 'F4'].includes(event.key)) {
 		await proCreate(6);
 		alert("Annoying ela teachers lol");
 	}
-	
+
 	return null;
+};
